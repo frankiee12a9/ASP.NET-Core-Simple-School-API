b5193a33036111c1afe8efaa5b4655c06e7fd2d1
Kien Nguyen The <cudayanh@gmail.com>
2021-10-03 11:32:19 +0900

    App extensions

---
+++ src/SchoolApiSrc/Extensions/Helper.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SchoolApiSrc.Extensions
+{
+    public static class Helpers
+    {
+        public static string ProfessorType = "Professor";
+        public static string TAType = "Teaching Assistant";
+        public static string StudentType = "Student";
+    }
+}
